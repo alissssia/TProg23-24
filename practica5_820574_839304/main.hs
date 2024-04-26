c1727ca8f97a41adee48989db09def8480d8dfb5
alissssia <820574@unizar.es>
2024-04-26 17:28:08 +0200

    Tarea 1 funciona

--- practica5_820574_839304/main.hs
+++ practica5_820574_839304/main.hs
@@ -1,5 +1,17 @@
-
 import SVG
 import LSystem
 
 -- funciones para probar que la practica funciona
+
+main = do
+-- dibuja un cuadrado
+    let cuadrado = tplot (1, 90, (0,0), 90) ">+>+>+>+"
+    savesvg "cuadrado" cuadrado
+
+-- dibuja un triangulo
+    let triangulo = tplot (1, 120, (0,0), 0) ">+>+>+"
+    savesvg "triangulo" triangulo
+
+-- dibuja un circulo
+    let circulo = tplot (0.1, 10, (0,0), 0) ">+>+>+>+>+>+>+>+>+>+>+>+>+>+>+>+>+>+>+>+>+>+>+>+>+>+>+>+>+>+>+>+>+>+>+>+"
+    savesvg "circulo" circulo
